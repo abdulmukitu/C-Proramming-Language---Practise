d602f708dd48dc81945ba6acc6c01f4678824f23
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-13 02:49:43 +0600

    Multiple Line Comment.

---
+++ 3rd Program.c
@@ -0,0 +1,14 @@
+/*Multiple Line Comment.*/
+/*
+This is a program that prints =>
+ 1.My name:
+ 2.My phone number:
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("My name is Abdul Mukit.");
+    printf("My phone number is 01741987819.");
+    return 0;
+}
