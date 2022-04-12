6750907abe235e9be6ae667a48185882129e6b61
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-13 02:50:39 +0600

    Example of Multiple Comment Line.

---
+++ 5th Program.c
@@ -0,0 +1,13 @@
+/*
+Example of Multiple Comment Line.
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("My name is Abdul Mukit.");
+    /*printf("I am 20 years old.");
+    printf("I am clumsy.");*/
+    printf("My phone number is 01741987819.");
+    return 0;
+}
