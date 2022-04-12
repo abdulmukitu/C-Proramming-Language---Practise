22deac83cecd605a02d3bb7dae64dc65ad9c1e90
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-13 03:11:29 +0600

    Uses of Escape Sequence, (\n) = Newline.
    (\n) is a Escape Sequence which produces an new line in C programming.

---
+++ 6th Program.c
@@ -0,0 +1,14 @@
+//Uses of Escape Sequence, (\n) = Newline.
+//(\n) produces one byte
+//(\n) is a Escape Sequence which produces an new line in C programming.
+
+#include<stdio.h>
+int main()
+{
+    printf("My name is Abdul Mukit.\n");
+    printf("I am 20 years old.\n");
+    printf("I am clumsy.\n");
+    printf("My phone number is 01741987819.\n");
+    return 0;
+}
+
