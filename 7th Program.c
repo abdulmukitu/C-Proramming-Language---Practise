8be0fab7d45c57afe2a2588896b9d920548f0e2b
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-13 03:17:15 +0600

    Example of (\n) = Newline.

---
+++ 7th Program.c
@@ -0,0 +1,8 @@
+//Example of (\n) = Newline.
+
+#include<stdio.h>
+int main()
+{
+    printf("My name is Abdul Mukit.\nI am 20 years old.\nI am clumsy.\nMy phone number is 01741987819.\n");
+    return 0;
+}
