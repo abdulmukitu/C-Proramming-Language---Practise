279979ab8294228206a474f2ee552605e9fa1d5c
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-13 20:29:19 +0600

    Uses of Data Type.(Keyword, Format Specifier)
    An integer variable number that will contain 10. Write a program that prints an integer number.

---
+++ 8th Program.c
@@ -0,0 +1,10 @@
+/*An integer variable num that will contain 10.
+Write a program that prints an integer number.*/
+//Uses of Data Type.(Keyword,Format Specifier)
+#include<stdio.h>
+int main()
+{
+    int num = 10;
+    printf("An integer number = %d",num);
+    return 0;
+}
