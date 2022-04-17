d9f2291727b3fcf71d1b3ba59e4b425a0e9c4c70
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-17 10:42:20 +0600

    Uses of Data Type.(Keyword, Format Specifier)
    Two integer variable numbers that will contain 10 and 20.
    Write a short program that prints two integer numbers.(Another short format)

---
+++ 11th Program.c
@@ -0,0 +1,11 @@
+/*Two integer variable numbers that will contain 10 and 20.
+Write a short program that prints two integer numbers.(Another format)*/
+//Uses of Data Type.(Keyword,Format Specifier)
+
+#include<stdio.h>
+int main()
+{
+    int num1 = 10,num2 = 20;
+    printf("Two integer numbers are %d and %d.\n",num1,num2);
+    return 0;
+}
