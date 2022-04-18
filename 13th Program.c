584ff9c22e03ea4321a6886a6f13f1910fc28579
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-18 23:18:47 +0600

    Uses of Data Type.(Keyword, Format Specifier, Variable)
    Write a short program that prints an integer number, a floating number, a double number and a character number.
    Update of 12th Program.

---
+++ 13th Program.c
@@ -0,0 +1,13 @@
+/*
+Write a short program that prints an integer number, a floating number, a double number and a character number.
+Update of 12th Program.
+*/
+//Uses of Data Type.(Keyword,Format Specifier,Variable)
+#include<stdio.h>
+int main()
+{
+    int num1 = 10;float num2 = 10.50;double num3 = 10.511111;char ch = 'a';
+
+    printf("Number 1 = %d\nNumber 2 = %f\nNumber 3 = %lf\nCharacter = %c\n",num1, num2, num3, ch);
+    return 0;
+}
